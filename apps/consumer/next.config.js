e9ed37c15097e8cdb1b2754a07b6f6aa379abf23
LoiNguyen94 <loinh@mioapp.vn>
2022-06-15 11:47:33 +0700

    singapore

--- apps/consumer/next.config.js
+++ apps/consumer/next.config.js
@@ -1,9 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
+const withPWA = require('next-pwa');
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  pwa: {
+    dest: 'public',
+  },
   images: {
     loader: 'custom',
     path: '',
@@ -58,4 +62,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withNx(nextConfig);
+module.exports = withPWA(withNx(nextConfig));
