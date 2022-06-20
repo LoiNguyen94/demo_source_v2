35cda9523d8533172d174c2a442262da6532142d
LoiNguyen94 <loinh@mioapp.vn>
2022-06-20 16:24:18 +0700

    f

--- apps/consumer/pages/products/details/DetailComponent.tsx
+++ apps/consumer/pages/products/details/DetailComponent.tsx
@@ -1,19 +1,20 @@
 import { Detail } from '@monorepo/ui-shares';
 import { TransitionLayout, Header } from '@monorepo/ui-shares';
-import Head from 'next/head';
 interface DetailProps {
   match?: { id: any };
   detail: any;
 }
 const DetailComponent = (props: DetailProps) => {
-  if (!props.detail) return null;
+  if(!props.detail) return null;
   return (
-    <>
-      <TransitionLayout>
-        <Header title="Chi tiết sản phẩm" />
-        <Detail detail={props.detail} />
-      </TransitionLayout>
-    </>
+    <TransitionLayout
+      title={props.detail?.name}
+      photo={props.detail?.photo}
+      description={props.detail?.log_time}
+    >
+      <Header title="Chi tiết sản phẩm" />
+      <Detail detail={props.detail} />
+    </TransitionLayout>
   );
 };
 
